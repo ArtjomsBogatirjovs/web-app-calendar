b2c80e4ce0b60ed7d3d670dae90461eb4ffc9360
ArtjomsBogatirjovs <artjoms.bogatirjovs@agroup.lv>
2023-11-03 10:20:20 +0200

    Initial commit

---
+++ front-calendar/src/ReservedList.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+
+const EntityList = () => {
+    const [entities, setEntities] = useState([]);
+
+    useEffect( () => {
+
+
+        const fetchData = async () => {
+            const response = await fetch("http://192.168.100.170:8080/calendar/reservations/get")
+            const responseJson = await response.json()
+
+            setEntities(responseJson)
+        }
+
+        fetchData()
+
+    }, []);
+
+    return (
+        <div className="reserved-list-container">
+            <h2>Reserved time</h2>
+            <ul>
+                {entities.map((entity, index) => (
+                    <li key={index}>
+                        {entity.date} {entity.playerOne} vs. {entity.playerTwo}  from {entity.start} to {entity.end}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default EntityList;
